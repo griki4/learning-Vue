be777175e3a6ec398393b8131023b26009132531
griki4 <95742896+griki4@users.noreply.github.com>
2022-06-22 16:59:05 +0800

    Add files via upload

---
+++ Vue_basic/19-单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <school></school>
+  <student></student>
+</div>
+</template>
+
+<script>
+import Student from "./Student";
+import School from "./School"
+export default {
+  name: "App",
+  components: {},
+
+  component:{
+    School,
+    Student
+  }
+}
+</script>
+
+<style>
+
+</style>
