54346387c7491282aaf7b404202c42ff69b76c91
griki4 <95742896+griki4@users.noreply.github.com>
2022-06-24 19:23:32 +0800

    Add files via upload

---
+++ vue_test/06 src scoped样式/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Student></Student>
+    <hr>
+    <School></School>
+  </div>
+</template>
+
+<script>
+import Student from "@/components/Student";
+import School from "@/components/School";
+
+
+export default {
+  name: "App",
+  components:{Student, School}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_test/06 src scoped样式/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h2>学校名字:{{name}}</h2>
+    <h2>学校地址:{{address}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      name:"HTI",
+      address:'haerbin'
+    }
+  }
+}
+</script>
+
+
+<!--scoped属性让样式只服务于当前组件，即使两个组件拥有相同的CSS样式，scoped的存在让他们不会冲突
+负责就会按照引入的顺序来覆盖层叠样式-->
+<style scoped>
+.demo {
+  background-color: skyblue;
+}
+</style>
