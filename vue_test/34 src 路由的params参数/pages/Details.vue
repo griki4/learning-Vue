5fa81937447ba9bc879a80469979413ccc0d16ab
griki4 <95742896+griki4@users.noreply.github.com>
2022-07-04 17:03:34 +0800

    Add files via upload

---
+++ vue_test/34 src 路由的params参数/pages/Details.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+<!--    路由组件中接收父组件传递过来的参数-->
+    <li>消息编号：{{$route.params.id}}</li>
+    <li>消息标题：{{$route.params.title}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Details"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_test/34 src 路由的params参数/pages/Message.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<ul>
+			<li v-for="m in messageList" :key="m.id">
+<!--将参数传递给params而不是query，使用params参数的时候不能使用对象形式的path写法，必须写成name方法-->
+<!--        <router-link :to="`/home/message/detail/${m.id}/${m.title}`"></router-link>-->
+				<router-link :to="{
+          name:'xiangqing',
+          params:{
+            id:m.id,
+            title:m.title
+          }
+				}">
+          {{m.title}}
+        </router-link>&nbsp;&nbsp;
+			</li>
+		</ul>
+    <hr>
+    <router-view></router-view>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Message',
+    data(){
+      return {
+        messageList:[
+          {id:'001', title:"消息001"},
+          {id:'002', title:"消息002"},
+          {id:'003', title:"消息003"}
+        ]
+      }
+    }
+	}
+</script>
