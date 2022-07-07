b1e63b904df0f5b5057ebda1d86935e71db39578
griki4 <95742896+griki4@users.noreply.github.com>
2022-07-07 17:39:23 +0800

    Add files via upload

---
+++ vue_test/39 src 全局路由守卫/pages/Details.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+<!--    路由组件中接收父组件传递过来的参数-->
+    <li>消息编号：{{id}}</li>
+    <li>消息标题：{{title}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Details",
+  props:['id', 'title']
+}
+</script>
+
+<style scoped>
+
+</style>
